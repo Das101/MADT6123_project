8c94505cc1a995f0432e2a2009c6533fe71c892d
Parth Vekariya <parthvekariya260898@gmail.com>
2023-03-29 16:35:23 -0400

    - ui design for login screen

---
+++ jobsearch/src/helper/colors.js
@@ -0,0 +1,10 @@
+export default colors = {
+    primary: '#0080FF',
+    secondary: '#3B3B3B',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#BEBEBE',
+    lightGray: '#F5F5F5',
+    darkGray: '#A9A9A9',
+  };
+  
--- jobsearch/src/screens/LoginScreen.js
+++ jobsearch/src/screens/LoginScreen.js
@@ -1,72 +1,133 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import colors from '../helper/colors';
 
+const { width, height } = Dimensions.get('window');
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    
-  };
+	const handleLogin = () => {
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Log in</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Log in</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.centerView}>
+				<Text style={styles.title}>Log in</Text>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.input}
+						placeholder="Email"
+						value={email}
+						onChangeText={setEmail}
+					/>
+				</View>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.input}
+						placeholder="Password"
+						value={password}
+						onChangeText={setPassword}
+						secureTextEntry={true}
+					/>
+				</View>
+				<TouchableOpacity onPress={handleLogin} style={styles.forgotButton}>
+					<Text style={styles.forgotText}>Forgot Password?</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={handleLogin}>
+					<Text style={styles.buttonText}>LOGIN</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={handleLogin} style={styles.registerButton}>
+					<Text>New Here? </Text><Text style={styles.forgotText}>Register</Text>
+				</TouchableOpacity>
+			</View>
+
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    padding: 10,
-    marginVertical: 10,
-    width: '100%',
-  },
-  button: {
-    backgroundColor: 'blue',
-    borderRadius: 4,
-    padding: 10,
-    marginVertical: 10,
-    width: '100%',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: colors.primary,
+		alignItems: 'center',
+		justifyContent: 'center',
+		alignContent: 'center',
+		padding: 20,
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginBottom: 20,
+		color: colors.primary,
+		marginLeft: -width * 0.55
+	},
+	centerView: {
+		width: width * 0.8,
+		height: width * 0.9,
+		alignItems: 'center',
+		justifyContent: 'center',
+		alignContent: 'center',
+		backgroundColor: colors.white,
+		borderRadius: 10,
+		shadowOffset: { width: 0, height: 0 },
+		shadowOpacity: 0.2,
+		shadowRadius: 5,
+		elevation: 4
+	},
+	input: {
+		borderRadius: 4,
+		padding: 10,
+		marginVertical: 5,
+		width: width * 0.7,
+	},
+	inputView: {
+		backgroundColor: colors.white,
+		borderRadius: 10,
+		shadowOffset: { width: 0, height: 0 },
+		shadowOpacity: 0.2,
+		shadowRadius: 5,
+		elevation: 4,
+		marginVertical: 10
+	},
+	forgotButton: {
+		width: width * 0.7,
+		alignItems: 'flex-end',
+		marginVertical: 6
+	},
+	forgotText: {
+		color: colors.black,
+		fontSize: 16,
+		fontWeight: 'bold',
+	},
+	button: {
+		backgroundColor: colors.primary,
+		borderRadius: 10,
+		shadowOffset: { width: 0, height: 0 },
+		shadowOpacity: 0.2,
+		shadowRadius: 5,
+		elevation: 4,
+		alignItems: 'center',
+		padding: 10,
+		marginVertical: 10,
+		width: width * 0.7,
+		height: width * 0.12,
+	},
+	registerButton: {
+		width: width * 0.7,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 10
+	},
+	buttonText: {
+		color: colors.white,
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginTop: 5
+	},
 });
 
 export default LoginScreen;
